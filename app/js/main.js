3261458750fb55da6ebe0469aedfd4b3bb2f2f9e
Asdez-4 <naj040315@gmail.com>
2020-02-24 00:17:00 +0200

    added content

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,16 @@
 $(function() {
-  $(".product__item-info-data-stars").rateYo({
+  var mixer = mixitup(".content__inner-box");
+
+  $(".stars-item").rateYo({
     rating: 4.6,
     readOnly: true,
     spacing: "3px",
     starWidth: "15px"
   });
+
   $(".product-slider").slick({
+    arrows: true,
     prevArrow: $(".product__top-arrows-left"),
-    nextArrow: $(".product__top-arrows-right"),
-    arrows: true
+    nextArrow: $(".product__top-arrows-right")
   });
 });
